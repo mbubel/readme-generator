b28e212dcc2822c22a9de018562fff5313663a1f
Michael <michaelbubel.1989@gmail.com>
2020-11-27 12:58:01 -0800

    Added a link to a video tutorial of the project, updated README and .gitignore

--- index.js
+++ index.js
@@ -52,28 +52,38 @@ const promptUser = () => {
 
 // README generator
 const generateREADME = (answers) =>
-`   TITLE:
+`TITLE:
 ${answers.title}
 
-    DESCRIPTION:
+DESCRIPTION:
 ${answers.description}
 
-    Table of Contents:
+Table of Contents:
+-Description
+-Installation
+-Usage
+-Contribution
+-License
+-Title
 
-    Installation:
+Installation:
 ${answers.install}
     
-    Usage:
+Usage:
 ${answers.usage}
 
-    Contribution:
+ Contribution:
 ${answers.contribution}
 
-    License:
+Test:
+${answers.test}
+https://www.awesomescreenshot.com/video/1877199?key=af9577f5b099f0aaaf2ee2b2f82d2a1c
+
+License:
 
 License MIT License Copyright (c) 2020 Michael Bubel. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
   
-    Questions:
+Questions?
 Github: ${answers.github}
 Email: ${answers.email}
   
