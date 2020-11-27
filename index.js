f07c21e05ddf608ad1603bc5455d41d458e7b2ee
Michael <michaelbubel.1989@gmail.com>
2020-11-27 12:19:02 -0800

    Added more questions to the README file and it automatically populates the answers

--- index.js
+++ index.js
@@ -4,20 +4,82 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// README prompts
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?",
+      name: "title",
+      message: "What is the title of the README?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please describe what this project will accomplish",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "How do you install this?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What are the uses for this project?",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "What are the contribution guidelines?",
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Please provide a link to the test",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub Account name?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your Email?",
     },
   ]);
 };
 
+// README generator
 const generateREADME = (answers) =>
-  `${answers.name}`;
+`   TITLE:
+${answers.title}
 
-// Bonus using async/await and try/catch
+    DESCRIPTION:
+${answers.description}
+
+    Table of Contents:
+
+    Installation:
+${answers.install}
+    
+    Usage:
+${answers.usage}
+
+    Contribution:
+${answers.contribution}
+
+    License:
+
+License MIT License Copyright (c) 2020 Michael Bubel. Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
+    Questions:
+Github: ${answers.github}
+Email: ${answers.email}
+  
+  
+  `;
+
 const init = async () => {
   console.log("README");
   try {
